d420eb4c17530e3106c033717fd22a6fee4da68b
Chloe Lim <chloelim22@gmail.com>
2025-01-18 21:15:44 +0800

    chore: frontend reorg base files

--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -1,1 +1,14 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_APP_NAME: string;
+  readonly VITE_API_PROTOCOL: string;
+  readonly VITE_API_BASE_URL: string;
+  readonly VITE_FRONTEND_PROTOCOL: string;
+  readonly VITE_FRONTEND_URL: string;
+  // more env variables...
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
