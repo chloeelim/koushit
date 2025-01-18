7203c31c410da1434acd93df64bd2b07a6204a0a
Chloe Lim <chloelim22@gmail.com>
2025-01-18 23:00:04 +0800

    style: linter error

--- frontend/src/app/routing/unauthenticated-app.tsx
+++ frontend/src/app/routing/unauthenticated-app.tsx
@@ -1,16 +1,13 @@
 import {
   createBrowserRouter,
   Navigate,
-  Outlet,
   RouterProvider,
 } from "react-router-dom";
 
 import Login from "@/app/pages/unauthenticated/login";
-import UnauthenticatedLayout from "@/components/layout/unauthenticated-layout";
 
 const unauthenticatedAppRouter = createBrowserRouter([
   {
-    element: <UnauthenticatedLayout children={<Outlet />} />,
     children: [
       {
         path: "/login",
